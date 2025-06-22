fd51b381cde71eabaaee246ad13fb7b2e3a2a97b
Nishant <official.nishant.ni8@gmail.com>
2025-06-22 16:14:21 +0530

    changes to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import stormanLogo from './assets/yinyang.svg'
 import './App.css'
 
 function App() {
@@ -10,10 +9,7 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={stormanLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
       <h1>Storman</h1>
@@ -22,12 +18,9 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          this app is in development.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
